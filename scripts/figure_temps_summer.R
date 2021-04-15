ffece21f3ff3e5a427d463780f77d5f486fdfcec
dustinkincaid <Dustin.Kincaid@uvm.edu>
2021-04-15 11:52:04 -0400

    Updated plot

--- scripts/figure_temps_summer.R
+++ scripts/figure_temps_summer.R
@@ -42,7 +42,7 @@ same_year <- function(x) {
   x
 }
   
-# Plot
+# Plot - all years plotted
 lc %>% 
   # Calculate daily temp means
   mutate(date = date(timestamp)) %>% 
@@ -54,7 +54,7 @@ lc %>%
   mutate(year = year(date),
          month = month(date)) %>% 
   # Select months to keep
-  filter(month %in% c(4, 5, 6, 7, 8, 9, 10)) %>% 
+  filter(month %in% c(4, 5, 6, 7, 8, 9, 10)) %>%
   # Plot
   ggplot(aes(x = same_year(date), y = temp_dailyMean_F, color = factor(year))) +
   geom_line() +
@@ -64,7 +64,48 @@ lc %>%
   ylab(expression(Water~temp.~at~88~ft.~(degree*F))) +
   theme_classic() +
   theme(axis.title.x = element_blank(),
-        legend.position = c(0.1, 0.8))
+        legend.position = c(0.1, 0.75))
 
 ggsave("plots/temps.png", height = 5, width = 5, units = "in", dpi = 150)
 
+
+# Plot - min, max, median & last 2 years
+# test <- lc %>% 
+#   # Calculate daily temp means
+#   mutate(date = date(timestamp)) %>% 
+#   group_by(date) %>% 
+#   summarize(temp_dailyMean = mean(Wtemp_Inst, na.rm = T)) %>% 
+#   # Convert temp from deg. C to F
+#   mutate(temp_dailyMean_F = temp_dailyMean/5*9+32) %>% 
+#   # Add year and month columns
+#   mutate(year = year(date),
+#          month = month(date),
+#          mday = mday(date),
+#          doy = yday(date)) %>% 
+#   select(-c(date, temp_dailyMean)) %>% 
+#   pivot_wider(names_from = year, values_from = temp_dailyMean_F) %>% 
+#   arrange(month, doy) %>% 
+#   rowwise() %>% 
+#   mutate(min = min(c_across(`2014`:ncol(.)), na.rm = T),
+#          max = max(c_across(`2014`:ncol(.)), na.rm = T),
+#          med = median(c_across(`2014`:ncol(.)), na.rm = T)) %>% 
+#   mutate(year = 2000,
+#          date = ymd(paste(year, month, mday))) %>%
+#   # select(-year) %>% 
+#   # select(date, everything()) %>% 
+#   # pivot_longer(cols = `2014`:ncol(.), names_to = "year", values_to = "temp_dailyMean_F") %>% 
+#   # # Select time series to plot
+#   # filter(year %in% c(`2020`, `2021`, "min", "max", "med"))
+#   # Select months to keep
+#   filter(month %in% c(3, 4, 5, 6, 7, 8, 9, 10, 11)) %>%
+#   # Plot
+#   ggplot(aes(x = date)) +
+#   geom_line(aes(y = min), linetype = "dashed")
+#   geom_line() +
+#   scale_color_brewer(name = "Year",
+#                      palette = "Accent") +
+#   # scale_color_discrete()
+#   ylab(expression(Water~temp.~at~88~ft.~(degree*F))) +
+#   theme_classic() +
+#   theme(axis.title.x = element_blank(),
+#         legend.position = c(0.1, 0.75))
